e330fac9394f2ce063381398c0154beb91a7293d
ohioskibidirizz123 <153636980+ohioskibidirizz123@users.noreply.github.com>
2025-01-23 11:05:26 -0600

    Create updateStatus.js

---
+++ updateStatus.js
@@ -0,0 +1,3 @@
+updateStatus("mySecret123", "green"); // This will set the status to "Up"
+updateStatus("mySecret123", "yellow"); // This will set the status to "Under Maintenance"
+updateStatus("mySecret123", "red"); // This will set the status to "Down"
